2dac57b2f5ce0ef80cb2150c14db713abafd2616
ESWARA RAO VETLA <112643496+ESWARVETLA-19@users.noreply.github.com>
2025-01-09 10:40:55 +0000

    added

---
+++ practice_project/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"example/project/notes"
+)
+
+func main() {
+	title, content := getNoteData()
+
+	userNote, err := note.New(title, content)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	userNote.Display()
+	err=userNote.Save()
+	if err!=nil{
+		fmt.Println("saving failed")
+		return 
+	}
+	fmt.Println("saved data successfully")
+
+}
+
+func getNoteData() (string, string) {
+	title := getUserInput("Note title:")
+	content := getUserInput("Note content:")
+
+	return title, content
+}
+
+func getUserInput(prompt string) string {
+	fmt.Printf("%v ", prompt)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	text, err := reader.ReadString('\n')
+
+	if err != nil {
+		return ""
+	}
+
+	text = strings.TrimSuffix(text, "\n")
+	text = strings.TrimSuffix(text, "\r")
+
+	return text
+}
